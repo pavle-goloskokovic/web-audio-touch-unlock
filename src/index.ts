55065881375654cc1195a1c4375a7c33837c9b6d
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-11-21 22:07:27 +0100

    Added type and error checking

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 export default function webAudioTouchUnlock (context: AudioContext)
 {
-    if (context && context.state === 'suspended')
+    if (!context || !(context instanceof ((<any>window).AudioContext || (<any>window).webkitAudioContext)))
+    {
+        console.error('webAudioTouchUnlock - You need to pass an instance of AudioContext to this method call!');
+        return;
+    }
+
+    if (typeof context.state !== 'string')
+    {
+        console.warn('webAudioTouchUnlock - Seems like this approach can not be used with current ' +
+            'implementation of AudioContext. We\'re sorry about that, however you can open an issue here: ' +
+            'https://github.com/pavle-goloskokovic/web-audio-touch-unlock/issues and we\'ll try to sort it out.');
+        return;
+    }
+
+    if (context.state === 'suspended')
     {
         let unlock = () =>
         {

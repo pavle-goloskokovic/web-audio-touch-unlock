57c80bb66c612a25b4928afe5d43456cbb7a1087
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-11-22 22:58:59 +0100

    Updated code so it uses Promises

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
+import * as Promise from 'promise'
+
 export default function webAudioTouchUnlock (context: AudioContext)
 {
-    if (!context || !(context instanceof ((<any>window).AudioContext || (<any>window).webkitAudioContext)))
+    return new Promise(function (resolve, reject)
     {
-        console.error('WebAudioTouchUnlock - You need to pass an instance of AudioContext to this method call!');
-        return;
-    }
-
-    if (typeof context.state !== 'string' || typeof context.resume !== 'function')
-    {
-        console.warn('WebAudioTouchUnlock - Seems like this approach can not be used with current ' +
-            'implementation of AudioContext. We\'re sorry about that, however you can open an issue here: ' +
-            'https://github.com/pavle-goloskokovic/web-audio-touch-unlock/issues and we\'ll try to sort it out.');
-        return;
-    }
-
-    if (context.state === 'suspended')
-    {
-        let unlock = () =>
+        if (!context || !(context instanceof ((<any>window).AudioContext || (<any>window).webkitAudioContext)))
         {
-            document.body.removeEventListener('touchstart', unlock);
+            reject('WebAudioTouchUnlock - You need to pass an instance of AudioContext to this method call!');
+            return;
+        }
 
-            context.resume();
-        };
+        if (typeof context.state !== 'string' || typeof context.resume !== 'function')
+        {
+            reject('WebAudioTouchUnlock - Seems like this approach can not be used with current ' +
+                'implementation of AudioContext. We\'re sorry about that, however you can open an issue here: ' +
+                'https://github.com/pavle-goloskokovic/web-audio-touch-unlock/issues and we\'ll try to sort it out.');
+            return;
+        }
 
-        document.body.addEventListener('touchstart', unlock, false);
-    }
+        if (context.state === 'suspended')
+        {
+            let unlock = () =>
+            {
+                document.body.removeEventListener('touchstart', unlock);
+
+                resolve(context.resume());
+            };
+
+            document.body.addEventListener('touchstart', unlock, false);
+        }
+    });
 }
